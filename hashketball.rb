0a72806115265772b0d62013ed13d008349fb7c6
Chelsey <cswilik@gmail.com>
2020-10-16 15:36:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,9 +185,7 @@ end
 
 
 def big_shoe_rebounds 
-  get_players.max_by do { |player| player[:shoe] } 
-  end 
-  [:player_name]
+  get_players.max_by { |player| player[:shoe] } [:player_name]
 end
     
 
